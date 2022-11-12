c760808e28593027acdff2e15d5432bedfb63734
Kamil Muzyka <62958895+kamilmuzyka91@users.noreply.github.com>
2022-11-12 16:26:39 +0000

    Obliczanie pola oraz obwodu koła.

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+// zadanie 3.1 - Pole i obwód koła
+#include <math.h>
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main() {
+  int radius;
+  float field_score;
+  float perimeter_score;
+
+  printf("\n");
+  printf(" ## Kalkulator pola i obwodu koła ## \n");
+  printf("\n");
+  printf("Wprowadz promien: ");
+  scanf("%d", &radius);
+
+  field_score = M_PI * (pow(radius, 2));
+  perimeter_score = 2 * M_PI * radius;
+
+  printf("Pole kola wynosi %0.2f\n", field_score);
+  printf("Obwod kola wynosi %0.2f\n", perimeter_score);
+
   return 0;
 }
